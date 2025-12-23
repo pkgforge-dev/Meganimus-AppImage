0731490b620f68d03efe8caf8828e3e0d5c71ebe
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-22 20:14:19 -0400

    get app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,6 +13,13 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+PKGBUILD=$(
+	wget https://api.github.com/repos/Kyuyrii/Meganimus/releases -O - \
+	  | sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*CachyOS.tar.gz"
+)
+wget "$PKGBUILD" -O /tmp/tar.gz
+tar -xvf /tmp/tar.gz
+cd ./Meganimus*CachyOS
+make-aur-package
 
 # If the application needs to be manually built that has to be done down here
